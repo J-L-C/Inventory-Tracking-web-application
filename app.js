1ee350e4db1a472faf7cc5d113d8d94def7c2c49
Abegail Taitano <79725435+abegailtaitano@users.noreply.github.com>
2022-05-12 15:17:21 -0500

    updated

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.get('/',(req,res) => {
+    res.send("Welcome to Abby\'s app!")
+  })
+
 app.use('/inventory',inventoryRoutes);
-app.listen(port, ()=> console.log(`we in ${port}`));
+app.listen(port, ()=> console.log(`Listening on ${port}`));
 
