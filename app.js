7b6f1186598c43ee44cfaf864eb561befda6f9d5
Abegail Taitano <79725435+abegailtaitano@users.noreply.github.com>
2022-05-12 15:23:31 -0500

    updated-app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 3000;
 
 app.get('/',(req,res) => {
-    res.send("Welcome to Abby\'s app!")
+    res.send("Welcome to Abby\'s Inventory Tracking web application!")
   })
 
 app.use('/inventory',inventoryRoutes);
